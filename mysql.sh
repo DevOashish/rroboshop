437328e28933d6a43ebc54facf5c09d452fc8386
DevOashish <ashich870@gmail.com>
2023-06-02 20:23:34 +0530

    latest changes

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
-yum module disable mysql -y 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>>> disable mysql lastest version <<<<<<<<\e[0m"
+dnf module disable mysql -y 
+
+echo -e "\e[36m>>>>>>>>> Copy to mysql repo <<<<<<<<\e[0m"
+cp /home/centos/rroboshop/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>> Install mysql-community <<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>> Start and enable mysql <<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld  
+
+echo -e "\e[36m>>>>>>>>> Reset the password of mysql <<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
