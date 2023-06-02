7d84f6517dcc8ba920a66d2fd321dde3a12593f8
DevOashish <ashich870@gmail.com>
2023-06-02 13:29:44 +0530

    latest changes

--- cart.sh
+++ cart.sh
@@ -1,35 +1,30 @@
-echo -e "\e[31m>>>>>>>>>>>>>>>>>>>> Config Node.js <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[32m>>>>>>>>>>>>>>>>>>>> Install Node.js <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo -e "\e[33m>>>>>>>>>>>>>>>>>>>> Add user <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
 useradd roboshop
 
-echo -e "\e[33m>>>>>>>>>>>>>>>>>>>> remove app <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
 rm -rf /app
+mkdir /app
 
-echo -e "\e[33m>>>>>>>>>>>>>>>>>>>> make dir <<<<<<<<<<<<<<<\e[0m"
-mkdir /app 
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
 
-echo -e "\e[34m>>>>>>>>>>>>>>>>>>>> cart zip <<<<<<<<<<<<<<<\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip 
-
-echo -e "\e[33m>>>>>>>>>>>>>>>>>>>> go to app dir <<<<<<<<<<<<<<<\e[0m"
-cd /app 
-
-
-echo -e "\e[35m>>>>>>>>>>>>>>>>>>>> unzip in dir <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
 unzip /tmp/cart.zip
 
-echo -e "\e[35m>>>>>>>>>>>>>>>>>>>> install npm <<<<<<<<<<<<<<<\e[0m"
-npm install 
+echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
+npm install
 
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> cp to cart service <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
 cp /home/centos/rroboshop/cart.service /etc/systemd/system/cart.service
 
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>> system enable and start <<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Start Cart Service <<<<<<<<\e[0m"
 systemctl daemon-reload
-systemctl enable cart 
+systemctl enable cart
 systemctl restart cart
