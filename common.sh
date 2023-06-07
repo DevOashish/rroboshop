737e2a1a63b684d6402b6dedaf1367d69dfc7081
DevOashish <ashich870@gmail.com>
2023-06-07 12:53:07 +0530

    if cond

--- common.sh
+++ common.sh
@@ -25,15 +25,15 @@ func_nodejs() {
   print_head "Configuring NodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  if [ "${schema_setup}" == "mysql" ]; then
-    print_head "Install MySQL Client"
-    yum install mysql -y
+  # if [ "${schema_setup}" == "mysql" ]; then
+  #   print_head "Install MySQL Client"
+  #   yum install mysql -y
 
-  print_head "Install NodeJS"
-  yum install nodejs -y
-  print_head "Load Schema"
-    mysql -h mysql-dev.devoash.tech -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-  fi
+  # print_head "Install NodeJS"
+  # yum install nodejs -y
+  # print_head "Load Schema"
+  #   mysql -h mysql-dev.devoash.tech -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+  # fi
 
 
   print_head "Add Application User"
@@ -66,23 +66,23 @@ func_nodejs() {
 
 }
 
-func_java() {
+# func_java() {
 
-yum install maven -y
+# yum install maven -y
 
-useradd ${app_user}
+# useradd ${app_user}
 
-rm -rf /app
-mkdir /app
+# rm -rf /app
+# mkdir /app
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
+# curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
 
-cd /app 
-unzip /tmp/shipping.zip
+# cd /app 
+# unzip /tmp/shipping.zip
 
-cd /app 
+# cd /app 
 
-mvn clean package 
-mv target/shipping-1.0.jar shipping.jar 
+# mvn clean package 
+# mv target/shipping-1.0.jar shipping.jar 
 
-}
+# }
