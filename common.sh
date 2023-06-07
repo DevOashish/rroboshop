230d0c8dc1a8f39b119bbd98e3393de1e063354c
DevOashish <ashich870@gmail.com>
2023-06-07 13:44:39 +0530

    if cond

--- common.sh
+++ common.sh
@@ -10,7 +10,7 @@ print_head() {
 
    if [ "$schema_setup" == "mongo" ]; then
     print_head "Copy MongoDB repo"
-    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    cp ${script_path}/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 
      print_head "Install MongoDB Client"
