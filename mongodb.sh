7db00b451ce383a8d5e75efdc8f553390513f828
DevOashish <ashich870@gmail.com>
2023-06-07 13:16:56 +0530

    if cond

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-cp ${script_path}mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org -y
 sudo sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod 
